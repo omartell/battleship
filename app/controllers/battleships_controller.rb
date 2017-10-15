4f1606aa0a383eb1d15599c0579c8e35845d234c
Oliver Martell <oliver.martell@gmail.com>
2017-10-15 15:19:17 +0100

    Report back missing sessions when recording shots

--- app/controllers/battleships_controller.rb
+++ app/controllers/battleships_controller.rb
@@ -9,6 +9,8 @@ module Battleship
   end
 
   def record_shot(session_id, position)
+    return "missing-session" unless battleships[session_id]
+
     battleships[session_id][:shots] ||= []
     battleships[session_id][:shots] << position
 
@@ -26,7 +28,6 @@ module Battleship
 end
 
 class BattleshipsController < ApplicationController
-
   def create
     positions = params.fetch(:positions)
     id        = Battleship.start(positions)
@@ -35,9 +36,13 @@ class BattleshipsController < ApplicationController
   end
 
   def update
-    id     = params.fetch(:id)
-    result = Battleship.record_shot(id, params.fetch(:position))
+    id      = params.fetch(:id)
+    message = Battleship.record_shot(id, params.fetch(:position))
 
-    render json: { message: result , links: { self: battleship_path(id) }}
+    if message == "missing-session"
+      render json: { message: message }, status: 404
+    else
+      render json: { message: message , links: { self: battleship_path(id) }}
+    end
   end
 end
--- spec/requests/battleship_spec.rb
+++ spec/requests/battleship_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Battleship, type: :request do
     expect(response.parsed_body).to include("links" => { "self" => a_string_matching(%r{/battleships/(\w|\d|-)+})})
   end
 
-  it "records sucessful shots for current battleships" do
+  it "reports sucessful shots for current battleships" do
     post battleships_path, params: { positions: [[0,3], [4,8], [6,6]]}, as: :json
 
     battleship = response.parsed_body
@@ -16,5 +16,12 @@ RSpec.describe Battleship, type: :request do
     put battleship.dig("links", "self"), params: { position: [0,3] }, as: :json
 
     expect(response.parsed_body).to include("message" => "hit")
+  end
+
+  it "reports missing sessions when recording a shot for a non-existent battleship" do
+    put battleship_path(SecureRandom::uuid), params: { position: [0,3] }, as: :json
+
+    expect(response.status).to eq(404)
+    expect(response.parsed_body).to include("message" => "missing-session")
   end
 end
