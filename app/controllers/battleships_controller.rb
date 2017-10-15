73bd5a425e04b37723aef1b3217acaf2bd28362f
Oliver Martell <oliver.martell@gmail.com>
2017-10-15 16:35:20 +0100

    Extract validation rules to separate object

--- app/controllers/battleships_controller.rb
+++ app/controllers/battleships_controller.rb
@@ -1,23 +1,40 @@
 module Battleship
+  class Validator
+    def self.validate_starting_positions(positions)
+      return { message: "invalid-initialization" } unless positions.present?
+      return { message: "invalid-initialization" } if positions.any? { |(x,y)|
+        (0..9).exclude?(x) || (0..9).exclude?(y)
+      }
+    end
+
+    def self.validate_shot(battleships, session_id, position)
+      return { message:"missing-session" } unless battleships[session_id]
+    end
+  end
+
   module_function
 
   def start(positions)
-    session_id      = SecureRandom::uuid
-    all_ships       = positions.map { |(x,y)| [[x,y], [x, y+1], [x,y+2]] }
-    battleships[session_id] = { ships: all_ships }
-    session_id
+    ships  = positions.flat_map { |(x,y)| [[x,y], [x, y - 1], [x,y - 2]] }
+    errors = Validator.validate_starting_positions(ships)
+    return errors if errors.present?
+
+    session_id = SecureRandom::uuid
+    battleships[session_id] = { ships: ships }
+
+    { message: "ok", session_id: session_id }
   end
 
   def record_shot(session_id, position)
-    return "missing-session" unless battleships[session_id]
+    errors = Validator.validate_shot(battleships, session_id, position)
+    return errors if errors.present?
 
     battleships[session_id][:shots] ||= []
     battleships[session_id][:shots] << position
+    intersection = battleships[session_id][:shots] & battleships[session_id][:ships]
 
-    if battleships[session_id][:shots] & battleships[session_id][:ships]
-      "hit"
-    else
-      "miss"
+    if intersection.present?
+      { message:"hit", session_id: session_id }
     end
   end
 
@@ -30,19 +47,24 @@ end
 class BattleshipsController < ApplicationController
   def create
     positions = params.fetch(:positions)
-    id        = Battleship.start(positions)
+    result   = Battleship.start(positions)
 
-    render json: { message: "OK", links: { self: battleship_path(id) } }
+    if result[:message] == "ok"
+      render json: { message: result[:message], links: { self: battleship_path(result[:session_id]) } }
+    else
+      render json: { message: result[:message] }, status: 400
+    end
   end
 
   def update
     id      = params.fetch(:id)
-    message = Battleship.record_shot(id, params.fetch(:position))
+    result  = Battleship.record_shot(id, params.fetch(:position))
+    message = result[:message]
 
-    if message == "missing-session"
-      render json: { message: message }, status: 404
-    else
+    if %w(hit).include?(message)
       render json: { message: message , links: { self: battleship_path(id) }}
+    else
+      render json: { message: message }, status: 400
     end
   end
 end
--- spec/requests/battleship_spec.rb
+++ spec/requests/battleship_spec.rb
@@ -1,11 +1,27 @@
 require "rails_helper"
 
 RSpec.describe Battleship, type: :request do
-  it "allows to initialize the position of the ships" do
-    post battleships_path, params: { positions: [[0,3], [4,8], [6,6]]}, as: :json
+  context "initialization" do
+    it "allows to initialize the position of the ships" do
+      post battleships_path, params: { positions: [[0,3], [4,8], [6,6]]}, as: :json
 
-    expect(response.parsed_body).to include("message" => "OK")
-    expect(response.parsed_body).to include("links" => { "self" => a_string_matching(%r{/battleships/(\w|\d|-)+})})
+      expect(response.parsed_body).to include("message" => "ok")
+      expect(response.parsed_body).to include("links" => { "self" => a_string_matching(%r{/battleships/(\w|\d|-)+})})
+    end
+
+    it "returns bad-request when not specifying a single position" do
+      post battleships_path, params: { positions: []}, as: :json
+
+      expect(response.status).to eq(400)
+      expect(response.parsed_body).to include("message" => "invalid-initialization")
+    end
+
+    it "returns bad-request when one of the positions is outside of 10x10" do
+      post battleships_path, params: { positions: [[10,10]]}, as: :json
+
+      expect(response.status).to eq(400)
+      expect(response.parsed_body).to include("message" => "invalid-initialization")
+    end
   end
 
   it "reports sucessful shots for current battleships" do
@@ -21,7 +37,7 @@ RSpec.describe Battleship, type: :request do
   it "reports missing sessions when recording a shot for a non-existent battleship" do
     put battleship_path(SecureRandom::uuid), params: { position: [0,3] }, as: :json
 
-    expect(response.status).to eq(404)
+    expect(response.status).to eq(400)
     expect(response.parsed_body).to include("message" => "missing-session")
   end
 end
